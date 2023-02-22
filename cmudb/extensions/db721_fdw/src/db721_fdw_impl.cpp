a8d3dbc6a98a850a35c7f13d66f210579a64d8a7
Arham Chopra <arhamschopra@gmail.com>
2023-02-22 17:36:13 -0500

    Add: Support for WHERE <CLAUSE(s)>

--- cmudb/extensions/db721_fdw/src/db721_fdw_impl.cpp
+++ cmudb/extensions/db721_fdw/src/db721_fdw_impl.cpp
@@ -51,7 +51,9 @@ extern "C" {
 #include "../../../../src/include/parser/parse_clause.h"
 #include "../../../../src/include/parser/parsetree.h"
 #include "../../../../src/include/partitioning/partprune.h"
+#include "../../../../src/include/utils/builtins.h"
 #include "../../../../src/include/utils/lsyscache.h"
+#include "../../../../src/include/utils/typcache.h"
 }
 // clang-format on
 
@@ -69,7 +71,7 @@ using namespace::std;
 #include <iostream>
 
 void debug(string str) {
-  elog(LOG, str.c_str());
+  cout << str << endl;
 }
 
 enum DataType {
@@ -83,7 +85,6 @@ DataType string_to_datatype(string enum_str) {
   if (enum_str == "int") return DataType::IntType;
   else if (enum_str == "float") return DataType::FloatType;
   else if (enum_str == "str") return DataType::StringType;
-  cout << "Error: Got datatype " << enum_str << endl;
   debug("Error: Got datatype " + enum_str);
   return NoneType;
 }
@@ -96,6 +97,20 @@ int data_type_to_size(DataType dt) {
   }
 }
 
+struct RowFilter {
+  Var* var;
+  Const *const_val;
+  int strategy;
+  bool neg;
+
+  RowFilter(Var* v, Const* cv, int strat, bool is_neg) {
+    var = v;
+    const_val = cv;
+    strategy = strat;
+    neg = is_neg;
+  }
+};
+
 struct db721_BlockMetadataInt {
   int max_val;
   int min_val;
@@ -103,9 +118,7 @@ struct db721_BlockMetadataInt {
   db721_BlockMetadataInt(JsonType json_obj):
     max_val(json_obj["max"]),
     min_val(json_obj["min"]),
-    num_val(json_obj["num"])
-  {
-  }
+    num_val(json_obj["num"]) { }
 
   void print() {
     cout << "MaxVal: " << this->max_val << endl;
@@ -208,6 +221,8 @@ struct db721_TableMetadata {
   int max_val_per_block;
   int num_rows;
 
+  map<AttrNumber, vector<RowFilter>> rfs;
+
   db721_TableMetadata () {
   }
 
@@ -254,43 +269,213 @@ struct db721_ScanState {
     cur_row(0) {}
 
   TupleTableSlot* next(TupleTableSlot* slot) {
-    if (cur_row == metadata->num_rows) {
-      return nullptr;
-    }
+    auto& rfs = metadata->rfs;
     auto* tuple_desc = slot->tts_tupleDescriptor;
+    // TODO: Fix bug to process the whole tuple each time
     for(int attr = 0; attr < tuple_desc->natts; ++attr) {
+      if (cur_row >= metadata->num_rows) {
+        return nullptr;
+      }
       AttrNumber attrnum = attr + 1 - FirstLowInvalidHeapAttributeNumber;
-      slot->tts_isnull[attrnum] = true;
+      slot->tts_isnull[attr] = true;
       auto col_name = string(NameStr(TupleDescAttr(tuple_desc, attr)->attname));
       if (attrs_used.find(attrnum) == attrs_used.end()) continue;
-      debug("Fetching column " + col_name);
+
+      auto& col_data = metadata->column_map[col_name];
+      fs.seekg(col_data.start_offset + cur_row * col_data.data_size);
+      Datum data;
+
+      switch (col_data.data_type) {
+        case IntType: {
+                        int val = 0;
+                        fs.read((char*)&val, col_data.data_size);
+                        data = Int32GetDatum(val);
+                        break;
+                      }
+        case FloatType: {
+                          float val = 0;
+                          fs.read((char*)&val, col_data.data_size);
+                          data = Float4GetDatum(val);
+                          break;
+                        }
+        case StringType: {
+                           text* t = (text*) palloc(col_data.data_size+VARHDRSZ);
+                           char val[33] = {};
+                           fs.read(val, col_data.data_size);
+                           data = CStringGetTextDatum(val);
+                           break;
+
+                         }
+      }
+
+      if (rfs.find(attr) != rfs.end()) {
+        // debug("Checking for " + col_name);
+        for(auto& rf: rfs[attr]) {
+
+          FmgrInfo finfo;
+          Datum    const_val = rf.const_val->constvalue;
+          int      collid = rf.const_val->constcollid;
+          int      strategy = rf.strategy;
+
+          auto* tce_c = lookup_type_cache(rf.const_val->consttype, TYPECACHE_BTREE_OPFAMILY);
+          auto* tce_v = lookup_type_cache(rf.var->vartype, TYPECACHE_BTREE_OPFAMILY);
+          auto cmp_proc_oid = get_opfamily_proc(tce_v->btree_opf, tce_c->btree_opintype, tce_v->btree_opintype, BTORDER_PROC);
+          // debug("Btree OpIntype are " + to_string(tce_v->btree_opintype) + " " + to_string(tce_c->btree_opintype));
+          fmgr_info(cmp_proc_oid, &finfo);
+          bool satisfies = false;
+
+          switch(rf.strategy) {
+            {
+              case BTLessStrategyNumber:
+                {
+                  int     cmpres;
+                  bool    satisfies;
+                  cmpres = FunctionCall2Coll(&finfo, collid, const_val, data);
+                  // debug(to_string(DatumGetFloat4(data)) + " BTLess " + to_string(DatumGetFloat8(const_val)) + " " + to_string(cmpres));
+
+                  satisfies = (strategy == BTLessStrategyNumber && cmpres > 0);
+                  if (rf.neg) {
+                    satisfies = !satisfies;
+                  }
+
+                  if (!satisfies) {
+                    attr = -1;
+                    cur_row++;
+                    debug("Skipping on BTLess(Eq) " + col_name);
+                    continue;
+                  }
+                  break;
+                }
+              case BTLessEqualStrategyNumber:
+                {
+                  int     cmpres;
+                  bool    satisfies;
+                  cmpres = FunctionCall2Coll(&finfo, collid, const_val, data);
+                  // debug(to_string(DatumGetFloat4(data)) + " BTLessEqual " + to_string(DatumGetFloat8(const_val)) + " " + to_string(cmpres));
+
+                  satisfies = (strategy == BTLessEqualStrategyNumber && cmpres >= 0);
+                  if (rf.neg) {
+                    satisfies = !satisfies;
+                  }
+
+                  if (!satisfies) {
+                    attr = -1;
+                    cur_row++;
+                    debug("Skipping on BTLess(Eq)" + col_name);
+                    continue;
+                  }
+                  break;
+                }
+
+              case BTGreaterStrategyNumber:
+                {
+                  int     cmpres;
+                  bool    satisfies;
+                  cmpres = FunctionCall2Coll(&finfo, collid, const_val, data);
+                  // debug(to_string(DatumGetFloat4(data)) + " BTGreater " + to_string(DatumGetFloat8(const_val)) + " " + to_string(cmpres));
+
+                  satisfies = (strategy == BTGreaterStrategyNumber && cmpres < 0);
+                  if (rf.neg) {
+                    satisfies = !satisfies;
+                  }
+
+                  if (!satisfies) {
+                    attr = -1;
+                    cur_row++;
+                    debug("Skipping on BTGreate(Eq)" + col_name);
+                    continue;
+                  }
+                  break;
+                }
+              case BTGreaterEqualStrategyNumber:
+                {
+                  int     cmpres;
+                  bool    satisfies;
+                  cmpres = FunctionCall2Coll(&finfo, collid, const_val, data);
+                  // debug(to_string(DatumGetFloat4(data)) + " BTGreaterEqual " + to_string(DatumGetFloat8(const_val)) + " " + to_string(cmpres));
+
+                  satisfies = (strategy == BTGreaterEqualStrategyNumber && cmpres <= 0);
+                  if (rf.neg) {
+                    satisfies = !satisfies;
+                  }
+
+                  if (!satisfies) {
+                    attr = -1;
+                    cur_row++;
+                    debug("Skipping on BTGreate(Eq)" + col_name);
+                    continue;
+                  }
+                  break;
+                }
+
+              case BTEqualStrategyNumber:
+                {
+                  int cmpres = FunctionCall2Coll(&finfo, collid, const_val, data);
+                  // auto tp_v = DatumGetVarCharP(data);
+                  // auto tp_c = DatumGetVarCharP(const_val);
+                  // debug(string(VARDATA(tp_v), VARSIZE(tp_v) - VARHDRSZ) + " BTEqual " + string(VARDATA(tp_c), VARSIZE(tp_c) - VARHDRSZ) + " " + to_string(cmpres));
+                  satisfies = (cmpres == 0);
+                  if (rf.neg) {
+                    satisfies = !satisfies;
+                  }
+
+                  if (!satisfies) {
+                    attr = -1;
+                    cur_row++;
+                    debug("Skipping on Equal(Eq)" + col_name);
+                    continue;
+                  }
+                  break;
+                }
+
+              default:
+                /* should not happen */
+                Assert(false);
+            }
+          }
+        }
+      }
+    }
+
+    // debug("Refetching results");
+
+    for(int attr = 0; attr < tuple_desc->natts; ++attr) {
+      AttrNumber attrnum = attr + 1 - FirstLowInvalidHeapAttributeNumber;
+      slot->tts_isnull[attr] = true;
+      auto col_name = string(NameStr(TupleDescAttr(tuple_desc, attr)->attname));
+      if (attrs_used.find(attrnum) == attrs_used.end()) continue;
+
       auto& col_data = metadata->column_map[col_name];
       fs.seekg(col_data.start_offset + cur_row * col_data.data_size);
       switch (col_data.data_type) {
         case IntType: {
-          int val = 0;
-          fs.read((char*)&val, col_data.data_size);
-          slot->tts_values[attr] = Int32GetDatum(val);
-          break;
-        }
+                        int val = 0;
+                        fs.read((char*)&val, col_data.data_size);
+                        slot->tts_values[attr] = Int32GetDatum(val);
+                        slot->tts_isnull[attr] = false;
+                        break;
+                      }
         case FloatType: {
-          float val = 0;
-          fs.read((char*)&val, col_data.data_size);
-          slot->tts_values[attr] = Float4GetDatum(val);
-          break;
-        }
+                          float val = 0;
+                          fs.read((char*)&val, col_data.data_size);
+                          slot->tts_values[attr] = Float4GetDatum(val);
+                          slot->tts_isnull[attr] = false;
+                          break;
+                        }
         case StringType: {
-          text* t = (text*) palloc(col_data.data_size+VARHDRSZ);
-          char data[32] = {};
-          fs.read(data, col_data.data_size);
-          SET_VARSIZE(t, col_data.data_size+VARHDRSZ);
-          memcpy(VARDATA(t), data, col_data.data_size);
-          slot->tts_values[attr] = PointerGetDatum(t);
-          break;
-        }
+                           text* t = (text*) palloc(col_data.data_size+VARHDRSZ);
+                           char data[32] = {};
+                           fs.read(data, col_data.data_size);
+                           SET_VARSIZE(t, col_data.data_size+VARHDRSZ);
+                           memcpy(VARDATA(t), data, col_data.data_size);
+                           slot->tts_values[attr] = PointerGetDatum(t);
+                           slot->tts_isnull[attr] = false;
+                           break;
+                         }
       }
     }
     cur_row++;
+
     return slot;
   }
 };
@@ -328,14 +513,103 @@ void get_foreign_table_info(Oid foreigntableid, db721_TableInfo &info) {
     ListCell *lc = &(opt_list->elements[i]);
     auto def = (DefElem*) lfirst(lc);
     if (def->defnamespace != nullptr ) {
-      cout << "Found namespace " << string(def->defnamespace) << endl;
     }
     if (string(def->defname) == "filename") {
       info.filename = defGetString(def);
-      cout << "Found filename " << info.filename << endl;
+      // cout << "Found filename " << info.filename << endl;
     } else if (string(def->defname) == "tablename") {
       info.tablename = defGetString(def);
-      cout << "Found tablename " << info.tablename << endl;
+      // cout << "Found tablename " << info.tablename << endl;
+    }
+  }
+}
+
+void extract_row_filters(RelOptInfo* baserel, map<AttrNumber, vector<RowFilter>> &rfs) {
+  ListCell* lc;
+  auto* all_clauses = baserel->baserestrictinfo;
+  foreach(lc, all_clauses) {
+    Expr *left, *right;
+    Var* v;
+    RelabelType* rt;
+    Const *c;
+    Oid opno;
+
+    Expr *clause = (Expr*) lfirst(lc);
+
+    if (IsA(clause, RestrictInfo)) {
+      clause = ((RestrictInfo*)clause)->clause;
+    }
+
+    if (IsA(clause, OpExpr)) {
+      auto *expr = (OpExpr*)clause;
+      if (list_length(expr->args) != 2) continue;
+
+      left = (Expr*) linitial(expr->args);
+      right = (Expr*) lsecond(expr->args);
+
+      if (IsA(right, Const))
+      {
+        if (IsA(left, RelabelType)) {
+          rt = (RelabelType*) left;
+          // debug("RelabelType Oid " + to_string(rt->resulttype) + " TypMod " + to_string(rt->resulttypmod));
+          auto arg_expr = rt->arg;
+          if (IsA(arg_expr, Var)) {
+            v = (Var*)arg_expr;
+            // debug("Var Oid " + to_string(v->vartype) + " TypMod " + to_string(v->vartypmod) + " Varratno " + to_string(v->varattno));
+          } else {
+            continue;
+          }
+        }
+        else if (IsA(left, Var)) {
+          v = (Var *) left;
+        } else {
+          continue;
+        }
+        c = (Const *) right;
+        opno = expr->opno;
+        debug("Const Op " + to_string(opno));
+      }
+      else if (IsA(left, Const))
+      {
+        if (IsA(right, RelabelType)) {
+          rt = (RelabelType*) right;
+          auto arg_expr = rt->arg;
+          if (IsA(arg_expr, Var)) {
+            v = (Var*)arg_expr;
+          } else {
+            continue;
+          }
+        }
+        else if (IsA(right, Var)) {
+          v = (Var *) right;
+        } else {
+          continue;
+        }
+        c = (Const *) left;
+        opno = get_commutator(expr->opno);
+      } else {
+        continue;
+      }
+
+      auto op_class = GetDefaultOpClass(v->vartype, BTREE_AM_OID);
+      if (not OidIsValid(op_class)) continue;
+      auto op_family = get_opclass_family(op_class);
+      int strategy = get_op_opfamily_strategy(opno, op_family);
+      if (strategy == 0) {
+        debug("BTREE_AM_OID not useful, trying negation of operator");
+        opno = get_negator(opno);
+        op_class = GetDefaultOpClass(v->vartype, BTREE_AM_OID);
+        if (not OidIsValid(op_class)) continue;
+        op_family = get_opclass_family(op_class);
+        strategy = get_op_opfamily_strategy(opno, op_family);
+        if (strategy == 0) continue;
+        rfs[v->varattno - 1].emplace_back(v, c, strategy, true);
+        debug("Adding NEG filter " + to_string(strategy) + " for Varratno " + to_string(v->varattno - 1));
+      } else {
+        rfs[v->varattno - 1].emplace_back(v, c, strategy, false);
+        debug("Adding filter " + to_string(strategy) + " for Varratno " + to_string(v->varattno - 1));
+      }
+      //
     }
   }
 }
@@ -345,24 +619,24 @@ pair<Cost, Cost> estimate_costs(PlannerInfo *root, RelOptInfo *baserel, Oid fore
 }
 
 void get_relevant_attrs(RelOptInfo* baserel) {
-    db721_QueryPlan* query_plan = (db721_QueryPlan*) baserel->fdw_private;
-    pull_varattnos((Node *) baserel->reltarget->exprs, baserel->relid, &query_plan->attrs_used);
-    ListCell *lc;
-    foreach(lc, baserel->baserestrictinfo)
-    {
-        RestrictInfo *rinfo = (RestrictInfo *) lfirst(lc);
-        pull_varattnos((Node *) rinfo->clause, baserel->relid, &query_plan->attrs_used);
-    }
+  db721_QueryPlan* query_plan = (db721_QueryPlan*) baserel->fdw_private;
+  pull_varattnos((Node *) baserel->reltarget->exprs, baserel->relid, &query_plan->attrs_used);
+  ListCell *lc;
+  foreach(lc, baserel->baserestrictinfo)
+  {
+    RestrictInfo *rinfo = (RestrictInfo *) lfirst(lc);
+    pull_varattnos((Node *) rinfo->clause, baserel->relid, &query_plan->attrs_used);
+  }
 
-    if (bms_is_empty(query_plan->attrs_used))
-    {
-        bms_free(query_plan->attrs_used);
-        query_plan->attrs_used = bms_make_singleton(1 - FirstLowInvalidHeapAttributeNumber);
-    }
+  if (bms_is_empty(query_plan->attrs_used))
+  {
+    bms_free(query_plan->attrs_used);
+    query_plan->attrs_used = bms_make_singleton(1 - FirstLowInvalidHeapAttributeNumber);
+  }
 }
 
 extern "C" void db721_GetForeignRelSize(PlannerInfo *root, RelOptInfo *baserel,
-                                      Oid foreigntableid) {
+    Oid foreigntableid) {
   db721_TableInfo info;
   get_foreign_table_info(foreigntableid, info);
 
@@ -388,8 +662,12 @@ extern "C" void db721_GetForeignRelSize(PlannerInfo *root, RelOptInfo *baserel,
     num_rows += cur_blk_rows;
   }
   metadata.num_rows = num_rows;
-  baserel->fdw_private = &metadata;
+  metadata.rfs.clear();
+  debug("extract_row_filters");
+  extract_row_filters(baserel, metadata.rfs);
+  debug("done");
 
+  baserel->fdw_private = &metadata;
   // TODO : Account of the "WHERE" filters
   baserel->rows = num_rows;
   baserel->reltarget->width = row_width;
@@ -397,7 +675,7 @@ extern "C" void db721_GetForeignRelSize(PlannerInfo *root, RelOptInfo *baserel,
 }
 
 extern "C" void db721_GetForeignPaths(PlannerInfo *root, RelOptInfo *baserel,
-                                    Oid foreigntableid) {
+    Oid foreigntableid) {
   debug("Getting paths!");
   Cost startup_cost = 0;
   Cost run_cost = 0;
@@ -410,21 +688,21 @@ extern "C" void db721_GetForeignPaths(PlannerInfo *root, RelOptInfo *baserel,
 
   tie(startup_cost, run_cost) = estimate_costs(root, baserel, foreigntableid);
   auto* foreign_path = (Path*) create_foreignscan_path(root,
-                                                       baserel,
-                                                       NULL,
-                                                       baserel->rows,
-                                                       startup_cost, startup_cost + run_cost,
-                                                       NULL,
-                                                       NULL,
-                                                       NULL,
-                                                       (List*) baserel->fdw_private);
+      baserel,
+      NULL,
+      baserel->rows,
+      startup_cost, startup_cost + run_cost,
+      NULL,
+      NULL,
+      NULL,
+      (List*) baserel->fdw_private);
   add_path(baserel, (Path*) foreign_path);
 }
 
 extern "C" ForeignScan *
 db721_GetForeignPlan(PlannerInfo *root, RelOptInfo *baserel, Oid foreigntableid,
-                   ForeignPath *best_path, List *tlist, List *scan_clauses,
-                   Plan *outer_plan) {
+    ForeignPath *best_path, List *tlist, List *scan_clauses,
+    Plan *outer_plan) {
   // TODO(721): Write me!
   // extern ForeignScan *make_foreignscan(List *qptlist, List *qpqual,
   //                    Index scanrelid, List *fdw_exprs, List *fdw_private,
@@ -440,7 +718,9 @@ db721_GetForeignPlan(PlannerInfo *root, RelOptInfo *baserel, Oid foreigntableid,
     ss->attrs_used.insert(attr);
   }
   baserel->fdw_private = ss;
-  return make_foreignscan(tlist, scan_clauses, baserel->relid, nullptr, (List*)(baserel->fdw_private), nullptr, nullptr, outer_plan);
+  auto n = make_foreignscan(tlist, nullptr, baserel->relid, nullptr, (List*)(baserel->fdw_private), nullptr, nullptr, outer_plan);
+  n->scan.plan;
+  return n;
 }
 
 extern "C" void db721_BeginForeignScan(ForeignScanState *node, int eflags) {
